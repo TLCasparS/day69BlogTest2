0182b1d80928539024b1099ea4483d094ae64d60
TLCasparS <112706704+TLCasparS@users.noreply.github.com>
2023-07-08 19:27:42 +0200

    Update main.py
    add postgresql database

--- main.py
+++ main.py
@@ -11,14 +11,17 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+#secret_key = SECRET_KEY
+
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
